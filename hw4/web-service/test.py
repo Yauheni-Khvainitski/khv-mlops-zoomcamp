9e21b5e28e36475608e3fd711ac95c32f465be21
Yauheni-Khvainitski <euheniy.khvoinitski@gmail.com>
2022-06-19 21:00:38 +0300

    [hw4] recap

--- hw4/web-service/test.py
+++ hw4/web-service/test.py
@@ -1,11 +1,16 @@
 import requests
-
+# import predict    # for local testing w/o flask app
 
 ride = {
     "PULocationID": 10,
     "DOLocationID": 50,
     "trip_distance": 40
 }
+
+# test without a running flask server
+# features = predict.prepare_features(ride)
+# preds = predict.predict(features)
+# print(preds)
 
 url = 'http://127.0.0.1:9696/predict'
 response = requests.post(url, json=ride)
