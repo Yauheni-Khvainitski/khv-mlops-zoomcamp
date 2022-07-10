5a71602fe203fd1f5a264dc9d9e7e81fca410d96
Yauheni-Khvainitski <euheniy.khvoinitski@gmail.com>
2022-07-10 22:36:38 +0300

    [hw6] 6.1

---
+++ hw6/code/lambda_function.py
@@ -0,0 +1,18 @@
+import os
+import model
+
+
+PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'ride_predictions')
+RUN_ID = os.getenv('RUN_ID', '22a623478ff84c878a59da6101e891da')
+TEST_RUN = os.getenv('TEST_RUN', 'False') == 'True'
+
+
+model_service = model.init(
+    prediction_sream_name=PREDICTIONS_STREAM_NAME,
+    run_id=RUN_ID,
+    test_run=TEST_RUN
+)
+
+
+def lambda_handler(event, context):
+    return model_service.lambda_handler(event)
