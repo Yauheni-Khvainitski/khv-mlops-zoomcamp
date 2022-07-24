9070138bd1bd2b037c44fd2d438b792676ba88a4
Yauheni-Khvainitski <euheniy.khvoinitski@gmail.com>
2022-07-24 21:05:37 +0300

    [hw6] 6b.2

--- hw6/code/infrastructure/main.tf
+++ hw6/code/infrastructure/main.tf
@@ -18,3 +18,12 @@ data "aws_caller_identity" "current_identity" {}
 locals {
   account_id = data.aws_caller_identity.current_identity.account_id
 }
+
+# ride_events
+module "source_kinesis_stream" {
+  source = "./modules/kinesis"
+  retention_period = 48
+  shard_count = 2
+  stream_name = "${var.source_stream_name}-${var.project_id}"
+  tags = var.project_id
+}
